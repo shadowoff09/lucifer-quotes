c9542d92b66c3399e663a9a77a702f35d846a4f5
Diogo Gaspar <diogogaspar931@gmail.com>
2021-12-29 01:39:08 +0000

    add static website

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 'use strict';
 
 var express = require('express');
+var path = require('path')
 var quotesRepository = require('./quotesRepository');
 var app = express();
 var port = process.env.PORT || 3001;
+
+app.use('/images', express.static('public/images'));
+app.use('/css', express.static('public/css'));
 
 app.all('*', function (req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
     next();
 });
 
+app.get("/", function (request, response){
+    //show this file when the "/" is requested
+    response.sendFile(__dirname+"/index.html");
+});
+
 app.get('/v1/quotes/:num?', function (req, res) {
     res.send(quotesRepository.getRandom(req.params.num || 1));
 });
+
 
 app.listen(port, function () {
     console.log('Server running on port', port);
